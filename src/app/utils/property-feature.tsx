f89635ddb6e8fe5cad04bdd31b2468b28eba7236
Eduardo <eduardohidalgo.dev@gmail.com>
2025-04-19 08:03:01 -0500

    fix: Fixed type of featureIconMap

--- src/app/utils/property-feature.tsx
+++ src/app/utils/property-feature.tsx
@@ -5,9 +5,18 @@ import {
   CarIcon,
   RulerIcon,
   ShowerHeadIcon,
+  LucideProps,
 } from "lucide-react";
+import { ForwardRefExoticComponent, RefAttributes } from "react";
 
-const featureIconMap: Record<Feature, React.FC> = {
+const featureIconMap: Partial<
+  Record<
+    Feature,
+    ForwardRefExoticComponent<
+      Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
+    >
+  >
+> = {
   [Feature.Rooms]: BedIcon,
   [Feature.Bathrooms]: BathIcon,
   [Feature.ParkingSlots]: CarIcon,
@@ -17,20 +26,9 @@ const featureIconMap: Record<Feature, React.FC> = {
 
 /** getFeatureIcons
  *
- * Generates a list of JSX elements representing property features with corresponding icons.
- * This utility is used to visually display property features such as rooms, bathrooms, parking slots, etc.
- *
- * @param {Array<PropertyFeature>} features - An array of property features, each containing a type and value.
- * @returns {JSX.Element[]} An array of JSX elements, each displaying an icon and the corresponding feature value.
- *
- * @example
- * ```tsx
- * const features = [
- *   { type: "Rooms", value: 3 },
- *   { type: "Bathrooms", value: 2 },
- * ];
- * const featureIcons = getFeatureIcons(features);
- * ```
+ * Generates a list of JSX elements representing property features with
+ * corresponding icons. This utility is used to visually display property
+ * features such as rooms, bathrooms, parking slots, etc.
  */
 export const getFeatureIcons = (features: Array<PropertyFeature>) => {
   return features.map((feature) => {
@@ -41,8 +39,8 @@ export const getFeatureIcons = (features: Array<PropertyFeature>) => {
         key={feature.type}
         className="flex items-center justify-center gap-2"
       >
-        <Icon />
-        <span className="text-sm text-black font-medium">{feature.value}</span>
+        {Icon && <Icon />}
+        <span className="text-sm font-medium text-black">{feature.value}</span>
       </div>
     );
   });
