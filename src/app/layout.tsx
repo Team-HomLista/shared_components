2224d06c4d35bb4cea28f4bbe4e89fcf8f2bd50f
Irving Caamal <irvv.17@gmail.com>
2025-08-21 10:19:10 -0500

    Add root layout component for Next.js app
    Introduces src/app/layout.tsx with global metadata, viewport settings, and font configuration. Sets up the main HTML structure and applies global styles for the application.

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata, Viewport } from "next";
+import { Inter } from "next/font/google";
+import { PropsWithChildren } from "react";
+import "@/styles/globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"]
+});
+
+export const metadata: Metadata = {
+  title: "HomLista",
+  icons: {
+    icon: "/favicon/favicon.ico",
+    shortcut: "/favicon/favicon.ico",
+    apple: "/favicon/apple-touch-icon.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png"
+      }
+    ]
+  }
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+};
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <html lang="es" suppressHydrationWarning={process.env.NODE_ENV === "production"}>
+      <body className={`${inter.variable} flex min-h-screen flex-col antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
