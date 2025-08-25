1e047fdd86eb488dd6e9f93f67c955dc1e02e105
Carlos León <carleon.dev@gmail.com>
2025-08-25 13:52:20 -0600

    feat: validate auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { fallbackLng, languages, cookieName, headerName } from "@/lib/i18n/settings";
 
+import { getCookie, setCookie } from "./lib/cookie";
+import { getAccessToken } from "./services/access-token";
+
 acceptLanguage.languages(languages);
 
 export const config = {
@@ -10,42 +13,49 @@ export const config = {
   matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)"]
 };
 
-export function middleware(req: NextRequest) {
+const requiredAuth = [
+  "/dashboard",
+  "/dashboard/(.*)",
+  "/profile",
+  "/profile/(.*)",
+  "/settings",
+  "/settings/(.*)"
+];
+
+export async function middleware(req: NextRequest) {
   if (req.headers.get("accept") === "text/x-component") return NextResponse.next();
-  // // Ignore paths with "icon" or "chrome"
-  // if (req.nextUrl.pathname.indexOf("icon") > -1 || req.nextUrl.pathname.indexOf("chrome") > -1)
-  //   return NextResponse.next();
 
-  let lng;
-  // Try to get language from cookie
-  if (req.cookies.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
-  // If no cookie, check the Accept-Language header
-  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
-  // Default to fallback language if still undefined
-  if (!lng) lng = fallbackLng;
+  const lng = await getLanguage(req);
+  req.headers.set(headerName, lng);
+  await setCookie(cookieName, lng);
 
-  // // Check if the language is already in the path
-  // const lngInPath = languages.find((loc) => req.nextUrl.pathname.startsWith(`/${loc}`));
-  // const headers = new Headers(req.headers);
-  // headers.set(headerName, lngInPath || lng);
-  const headers = new Headers(req.headers);
-  headers.set(headerName, lng);
-
-  // // If the language is not in the path, redirect to include it
-  // if (!lngInPath && !req.nextUrl.pathname.startsWith("/_next")) {
-  //   return NextResponse.redirect(
-  //     new URL(`/${lng}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-  //   );
-  // }
-
-  // If a referer exists, try to detect the language from there and set the cookie accordingly
-  if (req.headers.has("referer")) {
-    const refererUrl = new URL(req.headers.get("referer")!);
-    const lngInReferer = languages.find((l) => refererUrl.pathname.startsWith(`/${l}`));
-    const response = NextResponse.next({ headers });
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
-    return response;
+  if (!(await validAuthRoutes(req))) {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  return NextResponse.next({ headers });
+  return NextResponse.next();
+}
+
+async function getLanguage(req: NextRequest) {
+  const languageFromCookie = await getCookie(cookieName);
+
+  if (languageFromCookie) return languageFromCookie;
+
+  const languageFromHeader = acceptLanguage.get(req.headers.get("Accept-Language"));
+
+  if (languageFromHeader) return languageFromHeader;
+
+  return fallbackLng;
+}
+
+async function validAuthRoutes(req: NextRequest) {
+  const isAuthRoute = requiredAuth.some((route) => req.nextUrl.pathname.startsWith(route));
+
+  if (isAuthRoute) {
+    const accessToken = await getAccessToken();
+
+    return !!accessToken;
+  }
+
+  return true;
 }
