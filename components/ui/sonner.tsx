c08b4ad27e4b91e13de00ea721927ceebadbb301
Carlos León <carleon.dev@gmail.com>
2025-08-27 15:24:41 -0600

    refactor: update toaster style

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { AlertTriangle, CheckCircle, Info, Loader, XCircle } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
@@ -6,8 +9,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
 
   return (
     <Sonner
-      className="toaster group"
       theme={theme as ToasterProps["theme"]}
+      className="toaster group"
       style={
         {
           "--normal-bg": "var(--popover)",
@@ -15,6 +18,22 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-border": "var(--border)"
         } as React.CSSProperties
       }
+      toastOptions={{
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
+          actionButton: "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+          cancelButton: "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground"
+        }
+      }}
+      icons={{
+        success: <CheckCircle className="h-4 w-4 text-green-500" />,
+        info: <Info className="h-4 w-4 text-blue-500" />,
+        warning: <AlertTriangle className="h-4 w-4 text-amber-500" />,
+        error: <XCircle className="h-4 w-4 text-red-500" />,
+        loading: <Loader className="h-4 w-4 animate-spin text-gray-500" />
+      }}
       {...props}
     />
   );
