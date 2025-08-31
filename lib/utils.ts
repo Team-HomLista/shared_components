99eb22d6a4d2178d17d200915c5bda089a58b454
Mónica Roque Sánchez <44063035+monywy@users.noreply.github.com>
2025-08-31 18:15:25 -0500

    Update utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Obtiene la URL de embed de un video de YouTube a partir de su URL
+ */
+export function getYouTubeEmbedUrl(url: string) {
+  let videoId = "";
+  if (url.includes("youtube.com")) {
+    const params = new URLSearchParams(new URL(url).search);
+    videoId = params.get("v") || "";
+  } else if (url.includes("youtu.be")) {
+    videoId = url.split("/").pop() || "";
+  }
+  return `https://www.youtube.com/embed/${videoId}?controls=1`;
+}
